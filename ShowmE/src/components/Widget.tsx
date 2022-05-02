1c65e949cdb3db7d69c3e34e7d2ba17de5357391
lumiguel1 <luizmiguelreis2000@gmail.com>
2022-05-02 13:34:55 -0300

    Add Popover of HeadlessUI for Feedback Panel

--- ShowmE/src/components/Widget.tsx
+++ ShowmE/src/components/Widget.tsx
@@ -1,16 +1,22 @@
 import { ChatTeardropDots } from 'phosphor-react';
+import { Popover } from '@headlessui/react'
+
+
+import { useState } from 'react';
 
 export function Widget() {
     return (
-        <div className="absolute bottom-4 right-5">
-            <button className="flex items-center bg-brand-500 rounded-full p-3 h-12 text-white group">
+        <Popover className="absolute bottom-4 right-5">
+            <Popover.Panel>Eita me achou</Popover.Panel>
+
+            <Popover.Button className="flex items-center bg-brand-500 rounded-full p-3 h-12 text-white group">
                 <ChatTeardropDots className="w-6 h-6" />
 
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-300 ease-linear">
                     <span className="pl-2"></span>
                     Feedback   
                 </span>
-            </button>
-        </div>
+            </Popover.Button>
+        </Popover>
     )
 }
