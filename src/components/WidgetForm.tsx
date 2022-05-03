63eea7f285e7a5646736659a6de8df040f769f21
lumiguel1 <luizmiguelreis2000@gmail.com>
2022-05-03 11:23:20 -0300

    test branch

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -16,7 +16,7 @@ export function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
-                <span className="text-xl leading-6">Deixe seu feedbackA</span>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
 
                 <ButtonClose />
             </header>
@@ -26,7 +26,7 @@ export function WidgetForm() {
             </div>
             
             <footer className="text-xs text-neutral-400">
-                <a className="underline underline-offset-1" href="#">Vite app</a>
+                <a className="underline underline-offset-1" href="#">Vite appA</a>
             </footer>
         </div>
     )
