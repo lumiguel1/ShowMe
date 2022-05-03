d6e32d1c92ee2ba238485b3056ff47e7b44015e2
lumiguel1 <luizmiguelreis2000@gmail.com>
2022-05-03 12:05:22 -0300

    Added type and useState in WidgetForm

--- ShowmE/src/components/WidgetForm.tsx
+++ ShowmE/src/components/WidgetForm.tsx
@@ -2,7 +2,7 @@ import { ButtonClose } from "./ButtonClose";
 import bugImageUrl from '../assets/bug.svg';
 import ideaImageUrl from '../assets/idea.svg';
 import thoughtImageUrl from '../assets/thought.svg';
-import { useDebugValue } from "react";
+import { useState } from 'react';
 
 const feedTypes = {
     BUG: {
@@ -28,7 +28,11 @@ const feedTypes = {
     },
 }
 
+type FeedBackType = keyof typeof feedTypes
+
 export function WidgetForm() {
+    const [feedBackType, setFeedBackType] = useState<FeedBackType | null>(null);
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
@@ -40,7 +44,12 @@ export function WidgetForm() {
             <div className="flex py-8 gap-2 w-full">
                 { Object.entries(feedTypes).map(([key, value]) => {
                     return (
-                        <button>
+                        <button 
+                            key={key}
+                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                            type="button"
+                            onClick={() => setFeedBackType(key)}
+                        >
                             <img src={value.image.source} alt={value.image.alt} />
                             <span>{value.title}</span>
                         </button>
